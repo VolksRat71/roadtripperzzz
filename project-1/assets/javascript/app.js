a09b61d95433f76317c140c49f9b597e0a777b69
SamanthaLossett <samanthalossett@gmail.com>
2019-06-01 11:25:25 -0700

    with weather API working, new var in locations object, and render vacation working

--- project-1/assets/javascript/app.js
+++ project-1/assets/javascript/app.js
@@ -51,12 +51,6 @@ $(document).ready(function () {
         var userCity = $("#userCity").val().trim();
         var userState = $("#userState").val().trim();
         var userZipcode = $("#userZipcode").val().trim();
-        console.log(userFirstName);
-        console.log(userLastName);
-        console.log(userEmail);
-        console.log(userCity);
-        console.log(userState);
-        console.log(userZipcode);
         
         // form authentication
         if(userFirstName, userLastName, userEmail, userCity, userState, userZipcode === ""){
@@ -73,14 +67,38 @@ $(document).ready(function () {
         }else{
             audio.play(); 
             setInterval(function(){$(".loadingScreen").hide(); $("#pauseBtn").show(); $("#playBtn").show(); clearInterval(); $("#finalPage").show();}, 14600);
-        };;
-        
+        };
+
+        // weather API stuff DO NOT DELERTE ///
+        var location= locations[Math.floor(Math.random()* locations.length)];
+        var lati= location.lati;
+        var longi= location.longi;
+        console.log(location, lati, longi);
+        function weatherReport() {
+            var apiKey       = '998f56179b95a092c3fac99f4273aaab',
+            url          = 'https://api.darksky.net/forecast/'
+            queryURL     = "https://cors-anywhere.herokuapp.com/" + url + apiKey + "/" + lati + "," + longi;
+            console.log(queryURL);
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response){
+                console.log(response.daily.data);
+                for(var i=0; i<response.daily.data.length; i++){
+                    $("#weatherStuff").append("<h4>" + response.daily.data[i].summary + "</h4>")
+                }
+                
+            })
+        }
+
+        weatherReport()
+
+// this generates a random location //
         var renderVacation= function(){
             randomResult= locations[Math.floor(Math.random()* locations.length)];
             console.log(randomResult);
             $("#results-title").text("HERE ARE YOUR RANDOM ROADTRIP RESULTS!");
             $("#placeContent").text("Your Roadtrip Destination:" + " " + randomResult.place);
-            console.log(randomResult.place)
             $("#coordinatesContent").text("Coordinates:" + " " + randomResult.coordinates)
             $("#terrainContent").text("The Terrain:" + " " + randomResult.terrain);
             $("#descriptionContent").text("A Brief Description:" + " " + randomResult.description);
@@ -90,10 +108,9 @@ $(document).ready(function () {
             // adding the map to the on click function
             $('#results-container').empty();
             $('#map_frame').attr("src",mapUrl);
-            console.log(mapUrl);
+            // console.log(mapUrl);
         }
         renderVacation();
-        console.log('about to hide');
         $('#form').hide();
     $('#parnetMapContainer').show();
     
@@ -132,7 +149,8 @@ $(document).ready(function () {
         // Cam's locations
         {
             place: "Neahkahnie Mountain",
-            coordinates: "45.7440° N, 123.9410° W",
+            lati: "45.7440",
+            longi: "-123.9410",
             terrain: "Coastal Forest",
             description: "Neahkahnie Mountain is a mountain, or headland, on the Oregon Coast, north of Manzanita in Oswald West State Park overlooking U.S. Route 101. The peak is part of the Northern Oregon Coast Range, which is part of the Oregon Coast Range. It is best known for stories of Spanish treasure said to be buried either at the foot of the mountain, or on its slopes.",
             weather: "<script type='text/javascript' src='https://darksky.net/widget/default/45.7469,-123.9519/us12/en.js?width=100%&height=350&title=Neahkahnie Mountain&textColor=333333&bgColor=FFFFFF&transparency=false&skyColor=undefined&fontFamily=Default&customFont=&units=us&htColor=333333&ltColor=C7C7C7&displaySum=yes&displayHeader=yes'></script>",
@@ -140,7 +158,8 @@ $(document).ready(function () {
         },
         {
             place: "Crack in the Ground",
-            coordinates: "43.3336° N, 120.6723° W",
+            lati: "43.3336", 
+            longi: "-120.6723",
             terrain: "High Desert",
             description: "Crack in the Ground is a volcanic fissure about 2 miles (3.2 km) long  with depths measuring nearly 30 feet (9 m) below ground level in Central Oregon, United States. The eruptions from the Four Craters Lava Field were accompanied by a slight sinking of the older rock surface, forming a shallow, graben-like structure about 2 miles (3.2 km) wide and extending to the south into an old lake basin.",
             weather:  "<script type='text/javascript' src='https://darksky.net/widget/default/43.3327,-120.6712/us12/en.js?width=100%&height=350&title=Crack in the Ground&textColor=333333&bgColor=FFFFFF&transparency=false&skyColor=undefined&fontFamily=Default&customFont=&units=us&htColor=333333&ltColor=C7C7C7&displaySum=yes&displayHeader=yes'></script>",
@@ -148,7 +167,8 @@ $(document).ready(function () {
         },
         {
             place: "Metolius Balancing Rocks",
-            coordinates: " N 44° 34.672 W 121° 25.315.",
+            lati: "44.34.672",
+            longi: "-121.25315",
             terrain: "Desert",
             description: "These unique sculptures are the result of three distinctly welded volcanic tuffs settled upon one another and weathered away at different rates.",
             weather:"<script type='text/javascript' src='https://darksky.net/widget/default/44.5779,-121.4218/us12/en.js?width=100%&height=350&title=Metolius Balancing Rocks&textColor=333333&bgColor=FFFFFF&transparency=false&skyColor=undefined&fontFamily=Default&customFont=&units=us&htColor=333333&ltColor=C7C7C7&displaySum=yes&displayHeader=yes'></script>",
@@ -157,7 +177,8 @@ $(document).ready(function () {
         },
         {
             place: "Mount Hood",
-            coordinates: "45.3736° N, 121.6960° W",
+            lati: "45.3736", 
+            longi: "-121.6960",
             terrain: "Forest",
             description: "It is located about 50 miles (80 km) east-southeast of Portland, on the border between Clackamas and Hood River counties. In addition to being Oregon's highest mountain, it is one of the loftiest mountains in the nation based on its prominence.",
             weather: "<script type='text/javascript' src='https://darksky.net/widget/default/45.3735,-121.6959/us12/en.js?width=100%&height=350&title=Mount Hood&textColor=333333&bgColor=FFFFFF&transparency=false&skyColor=undefined&fontFamily=Default&customFont=&units=us&htColor=333333&ltColor=C7C7C7&displaySum=yes&displayHeader=yes'></script>",
@@ -166,7 +187,8 @@ $(document).ready(function () {
         },
         {
             place: "Crater Lake",
-            coordinates: "42.9446° N, 122.1090° W",
+            lati: "42.9446", 
+            longi: "-122.1090",
             terrain: "Forest",
             description: "Crater Lake is a crater lake in south-central Oregon in the western United States. It is the main feature of Crater Lake National Park and is famous for its deep blue color and water clarity. ",
             weather: "<script type='text/javascript' src='https://darksky.net/widget/default/42.9415,-122.0988/us12/en.js?width=100%&height=350&title=Crater Lake&textColor=333333&bgColor=FFFFFF&transparency=false&skyColor=undefined&fontFamily=Default&customFont=&units=us&htColor=333333&ltColor=C7C7C7&displaySum=yes&displayHeader=yes'></script>",
@@ -176,7 +198,8 @@ $(document).ready(function () {
         // Sam's locations
         {
             place: "Painted Hills",
-            coordinates: "44.6615° N, 120.2730° W",
+            lati: "44.6615",
+            longi:"-120.2730",
             terrain: "Desert",
             description: "is one of the three units of the John Day Fossil Beds National Monument, located in Wheeler County, Oregon.Painted Hills is named after the colorful layers of its hills corresponding to various geological eras, formed when the area was an ancient river floodplain.",
             weather: "<script type='text/javascript' src='https://darksky.net/widget/default/44.6476,-120.2607/us12/en.js?width=100%&height=350&title=Painted Hills&textColor=333333&bgColor=FFFFFF&transparency=false&skyColor=undefined&fontFamily=Default&customFont=&units=us&htColor=333333&ltColor=C7C7C7&displaySum=yes&displayHeader=yes'></script>",
@@ -184,7 +207,8 @@ $(document).ready(function () {
         },
         {
             place: "Smith Rock",
-            coordinates: "44.3682°N, 121.1406°W",
+            lati: "44.3682",
+            longi: "-121.1406",
             terrain: "Desert",
             description: "is an American state park located in central Oregon's High Desert near the communities of Redmond and Terrebonne. Its sheer cliffs of tuff and basalt are ideal for rock climbing of all difficulty levels. Smith Rock is generally considered the birthplace of modern American sport climbing, and is host to cutting-edge climbing routes. It is popular for sport climbing, traditional climbing, multi-pitch climbing, and bouldering.",
             weather: "<script type='text/javascript' src='https://darksky.net/widget/default/44.3638,-121.1466/us12/en.js?width=100%&height=350&title= smith rock, oregon&textColor=333333&bgColor=FFFFFF&transparency=false&skyColor=undefined&fontFamily=Default&customFont=&units=us&htColor=333333&ltColor=C7C7C7&displaySum=yes&displayHeader=yes'></script>",
@@ -193,7 +217,8 @@ $(document).ready(function () {
         },
         {
             place: "Ashland",
-            coordinates: "42.1946°N, 122.7095°W,",
+            lati: "42.1946",
+            longi: "-122.7095",
             terrain: "City",
             description: "is a city in Oregon with a poplulation estimated at 21,000, home to Southern Oregon University and the infamous Shakespeare festival.Enjoy the arts, food, and liverl culture of this awesome city!",
             weather: "<script type='text/javascript' src='https://darksky.net/widget/default/42.1972,-122.7154/us12/en.js?width=100%&height=350&title=Ashland, or&textColor=333333&bgColor=FFFFFF&transparency=false&skyColor=undefined&fontFamily=Default&customFont=&units=us&htColor=333333&ltColor=C7C7C7&displaySum=yes&displayHeader=yes'></script>",
@@ -202,7 +227,8 @@ $(document).ready(function () {
         },
         {
             place: "Cougar Resovoir Hot Springs",
-            coordinates: "44.0830° N, 122.2384° W",
+            lati: "44.0830",
+            longi: "122.2384",
             terrain: "Forest",
             description: "Terwilliger Hot Springs, also known as Cougar Hot Springs, are geothermal pools in the Willamette National Forest in the U.S.state of Oregon, 53 miles(85 km) east of Eugene.",
             weather: "<script type='text/javascript' src='https://darksky.net/widget/default/45.3012,-122.947/us12/en.js?width=100%&height=350&title=Cougar Resovoir Hot Springs, Oregon&textColor=333333&bgColor=FFFFFF&transparency=false&skyColor=undefined&fontFamily=Default&customFont=&units=us&htColor=333333&ltColor=C7C7C7&displaySum=yes&displayHeader=yes'></script>",
@@ -211,7 +237,8 @@ $(document).ready(function () {
         },
         {
             place: "Portland",
-            coordinates: "45.5155° N, 122.6793° W",
+            lati: "45.5155",
+            longi: "-122.6793",
             terrain: "Large city",
             description: "is the largest city in the U.S. state of Oregon and has approximately 2.4 million people that live in the Portland metropolitan statistical.  Portland is home to the NBA Trail Blazers, a breadth of art and ServiceUIFrameContext, and plenty of outdoor advenrtures.",
             weather: "<script type='text/javascript' src='https://darksky.net/widget/default/45.5202,-122.6742/us12/en.js?width=100%&height=350&title=Portland, Oregon&textColor=333333&bgColor=FFFFFF&transparency=false&skyColor=undefined&fontFamily=Default&customFont=&units=us&htColor=333333&ltColor=C7C7C7&displaySum=yes&displayHeader=yes'></script>",
@@ -221,7 +248,8 @@ $(document).ready(function () {
         // Stesha's locations
         {
             place: "West Eagle Meadow Campground (Wallowa-Whitman National Forest)",
-            coordinates: "45°20′00″N, 117°00′05″W",
+            lati: "45.20′00",
+            longi: "-117.00′05",
             terrain: "Forest",
             description: "The Wallowa–Whitman National Forest is used for hiking, camping, fishing, hunting, and other recreational activities.",
             weather: "<script type='text/javascript' src='https://darksky.net/widget/default/45.4224,-117.6177/us12/en.js?width=100%&height=350&title=Wallowa-Whitman National Forest, Oregon&textColor=333333&bgColor=FFFFFF&transparency=false&skyColor=undefined&fontFamily=Default&customFont=&units=us&htColor=333333&ltColor=C7C7C7&displaySum=yes&displayHeader=yes'></script>",
@@ -230,7 +258,8 @@ $(document).ready(function () {
         },
         {
             place: "Dixie Campground (Malheur National Forest)",
-            terrain: "44.534823 N, 118.5879 W",
+            lati: "44.534823",
+            longi:  "-118.5879",
             terrain: "Wooded (Ponderosa)",
             description: "This is a great destination for berry picking, it is also a wonderful base camp for anyone wanting to hike, hunt or just explore the Malheur National Forest.",
             weather: "<script type='text/javascript' src='https://darksky.net/widget/default/44.2049,-119.327/us12/en.js?width=100%&height=350&title=Malheur National Forest, Oregon&textColor=333333&bgColor=FFFFFF&transparency=false&skyColor=undefined&fontFamily=Default&customFont=&units=us&htColor=333333&ltColor=C7C7C7&displaySum=yes&displayHeader=yes'></script>",
@@ -239,7 +268,8 @@ $(document).ready(function () {
         },
         {
             place: "Haystack Rock, Cannon Beach",
-            coordinates: "43.0861°N, 124.4383°W",
+            lati: "43.0861",
+            longi: "-124.4383",
             terrain: "Beach",
             description: "Visitors to Haystack Rock can view many species of marine wildlife in their natural habitat during low tide.",
             weather: "<script type='text/javascript' src='https://darksky.net/widget/default/45.8844,-123.9684/us12/en.js?width=100%&height=350&title=Haystack Rock, Cannon Beach, Oregon&textColor=333333&bgColor=FFFFFF&transparency=false&skyColor=undefined&fontFamily=Default&customFont=&units=us&htColor=333333&ltColor=C7C7C7&displaySum=yes&displayHeader=yes'></script>",
@@ -248,8 +278,9 @@ $(document).ready(function () {
         },
         // Nate's Locations
         {
-            place: "Devils Punch Bowl",
-            coordinates: "44.7472°N, 122.0637°W",
+            name: "Devils Punch Bowl",
+            lati: "44.7472",
+            longi: "-122.0637",
             terrain: "Coast",
             description: "Along the beautiful Oregon Coast. It is centered on a large bowl naturally carved in a rock headland which is partially open to the Pacific Ocean.",
             weather: "<script type='text/javascript' src='https://darksky.net/widget/default/43.7474,-118.0841/us12/en.js?width=100%&height=350&title=Devils Punch Bowl, Oregon&textColor=333333&bgColor=FFFFFF&transparency=false&skyColor=undefined&fontFamily=Default&customFont=&units=us&htColor=333333&ltColor=C7C7C7&displaySum=yes&displayHeader=yes'></script>",
@@ -257,8 +288,9 @@ $(document).ready(function () {
 
         },
         {
-            place: "Multnomah Falls",
-            coordinates: "45.5762°N, 122.1158°W",
+            name: "Multnomah Falls",
+            lati: "45.5762",
+            longi: "-122.1158",
             terrain: "Forest",
             description: "Two consecutive waterfalls & beautiful hiking with a bridge along the Columbia Gorge. Visit the Multnomah Falls lodge built in 1925.",
             weather: "<script type='text/javascript' src='https://darksky.net/widget/default/45.5759,-122.1155/us12/en.js?width=100%&height=350&title=Multnomah Falls, Oregon&textColor=333333&bgColor=FFFFFF&transparency=false&skyColor=undefined&fontFamily=Default&customFont=&units=us&htColor=333333&ltColor=C7C7C7&displaySum=yes&displayHeader=yes'></script>",
@@ -266,8 +298,9 @@ $(document).ready(function () {
 
         },
         {
-            place: "Oregon Badlands Wilderness",
-            coordinates: "44.0112°N, 121.0009°W",
+            name: "Oregon Badlands Wilderness",
+            lati: "44.0112",
+            longi: "-121.0009",
             terrain: "Desert",
             description: "The area is known for igneous castle-like rock formations, harsh terrain, ancient Juniper trees, sagebrush, and extensive arid land. A great place to hike and camp.",
             weather: "<script type='text/javascript' src='https://darksky.net/widget/default/44.0033,-121.0231/us12/en.js?width=100%&height=350&title=Oregon Badlands Wilderness, Oregon&textColor=333333&bgColor=FFFFFF&transparency=false&skyColor=undefined&fontFamily=Default&customFont=&units=us&htColor=333333&ltColor=C7C7C7&displaySum=yes&displayHeader=yes'></script>",
